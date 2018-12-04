22213366350f75191ae7e6ae2a5811bf361ae584
Michael Maramba <mpmaramb@calpoly.edu>
2018-12-03 19:46:20 -0800

    mvp train data

--- Scrape.py
+++ Scrape.py
@@ -17,8 +17,7 @@ def findCandidates():
 	    players.append(player)
 
 	# Create list of URLs based on identifiers
-	url_list = ['https://www.basketball-reference.com/players/' + player[0] + 
-				'/' + player + '.html' for player in players]
+	url_list = ['https://www.basketball-reference.com/players/' + player[0] + '/' + player + '.html' for player in players]
 
 	return url_list
 
@@ -43,6 +42,7 @@ def writeStatsToCSV(csvfile, url_list):
 
 		# Individual  stats
 		row = soup.find("tr", { "id" : "per_game.2019" })
+
 		age = row.find("td", { "data-stat" : "age" }).text
 		games = row.find("td", { "data-stat" : "g" }).text
 		mins = row.find("td", { "data-stat" : "mp_per_g" }).text
@@ -60,17 +60,63 @@ def writeStatsToCSV(csvfile, url_list):
 		winshare = winshare_table.findAll("p")[2].text
 		
 		# Write to CSV
-		info = [name,age,games,mins,points,rebs,assists,steals,blocks,fg_pct,fg3_pct,ft_pct,winshare]
+		info = [name, age, games, mins, points, rebs, assists, steals, blocks, fg_pct, fg3_pct, ft_pct, winshare]
+		writer.writerow(info)
+
+def getVotingData(csvfile):
+
+	# Set up CSV file
+	fieldnames = ['Player', 'Age', 'G', 'MP', 'PTS', 'TRB', 'AST', 'STL',
+	              'BLK', 'FG%', '3P%', 'FT%', 'WS', 'Share']
+
+	writer = csv.writer(csvfile)
+	writer.writerow(fieldnames)
+
+	url_list = ['http://www.basketball-reference.com/awards/awards_' +  str(x) + '.html' for x in range(2018, 1980, -1)]
+
+	for url in url_list:
+
+		# Set up BS4 with the URL
+		response = requests.get(url)
+		soup = BeautifulSoup(response.content, "html.parser")
+
+		# MVP Table
+		mvps = soup.find("div", { "id" : "div_mvp" })
+
+		# Similar to other function
+		name = mvps.find("td", { "class" : "left " }).text
+		age = mvps.find("td", { "data-stat" : "age" }).text
+		games = mvps.find("td", { "data-stat" : "g" }).text
+		mins = mvps.find("td", { "data-stat" : "mp_per_g" }).text
+		points = mvps.find("td", { "data-stat" : "pts_per_g" }).text
+		rebs = mvps.find("td", { "data-stat" : "trb_per_g" }).text
+		assists = mvps.find("td", { "data-stat" : "ast_per_g" }).text
+		steals = mvps.find("td", { "data-stat" : "stl_per_g" }).text
+		blocks = mvps.find("td", { "data-stat" : "blk_per_g" }).text
+		fg_pct = mvps.find("td", { "data-stat" : "fg_pct" }).text
+		fg3_pct = mvps.find("td", { "data-stat" : "fg3_pct" }).text
+		ft_pct = mvps.find("td", { "data-stat" : "ft_pct" }).text
+
+		winshare = mvps.find("td", { "data-stat" : "ws" }).text
+		voteshare = mvps.find("td", { "data-stat" : "award_share" }).text
+
+		info = [name, age, games, mins, points, rebs, assists, steals, blocks, fg_pct, fg3_pct, ft_pct, winshare, voteshare]
 		writer.writerow(info)
 
 
 
-
 def main():
+	'''
 	candidates = findCandidates()
 
 	with open('players.csv', 'w') as csvfile:
 		writeStatsToCSV(csvfile, candidates)
+	'''
+
+	with open('mvp.csv', 'w') as trainfile:
+		getVotingData(trainfile)
+
+
 
 
 if __name__ == "__main__":
